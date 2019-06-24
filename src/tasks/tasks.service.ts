49958a7169cb7aa8df4fc771ef32ba13df384090
Everton Pereira <everton.mwp@gmail.com>
2019-06-24 19:27:09 -0300

    refactor deleteTask method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -43,10 +43,13 @@ export class TasksService {
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskRepository.createTask(createTaskDto)
   }
-  // deleteTask(id: string): void {
-  //   const found = this.getTaskById(id)
-  //   this.tasks = this.tasks.filter(task => task.id !== found.id)
-  // }
+
+  async deleteTask(id: number): Promise<void> {
+    const result = await this.taskRepository.delete(id)
+    if (!result.affected) {
+      throw new NotFoundException(`Task with ID: ${id} was not found`)
+    }
+  }
   // updateTaskStatus(id: string, status: TaskStatus): Task {
   //   const task = this.getTaskById(id)
   //   task.status = status
