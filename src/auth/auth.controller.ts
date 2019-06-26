803550881055a5252565be59b81cdf8b11e6f4fa
Everton Pereira <everton.mwp@gmail.com>
2019-06-25 23:57:45 -0300

    add validation to duplicate usernames

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common'
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  InternalServerErrorException,
+} from '@nestjs/common'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 import { AuthService } from './auth.service'
+import { ConflictException } from '@nestjs/common'
 
 @Controller('auth')
 export class AuthController {
@@ -10,6 +17,14 @@ export class AuthController {
   async signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<void> {
-    this.authService.signUp(authCredentialsDto)
+    try {
+      await this.authService.signUp(authCredentialsDto)
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists.')
+      } else {
+        throw new InternalServerErrorException()
+      }
+    }
   }
 }
